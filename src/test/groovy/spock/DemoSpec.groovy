fe77d272d22a40585df53d7b15b902d3c5cb22c4
jianbao <jianbao@yahoo-inc.com>
2014-04-02 11:10:23 -0700

    minor changes

--- src/test/groovy/spock/DemoSpec.groovy
+++ src/test/groovy/spock/DemoSpec.groovy
@@ -7,28 +7,28 @@ import spock.lang.Specification
 class DemoSpec extends Specification {
 
     def one() {
-        println System.getProperty('org.gradle.test.worker')
+        println System.getProperty('org.gradle.test.worker') + ": " + new Date().format('HH:mm:ss')
         Thread.sleep( 100 )
         expect:
         1 == 1
     }
 
     def two() {
-        println System.getProperty('org.gradle.test.worker')
+        println System.getProperty('org.gradle.test.worker') + ": " + new Date().format('HH:mm:ss')
         Thread.sleep( 100 )
         expect:
         1 == 1
     }
 
     def three() {
-        println System.getProperty('org.gradle.test.worker')
+        println System.getProperty('org.gradle.test.worker') + ": " + new Date().format('HH:mm:ss')
         Thread.sleep( 100 )
         expect:
         1 == 1
     }
 
     def four() {
-        println System.getProperty('org.gradle.test.worker')
+        println System.getProperty('org.gradle.test.worker') + ": " + new Date().format('HH:mm:ss')
         Thread.sleep( 100 )
         expect:
         1 == 1
